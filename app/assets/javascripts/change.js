81df0ba74957833dd2bd2470d0ce3536da8a65c8
shwe-yuji <okumura19943210yuji@gmail.com>
2020-08-23 17:45:22 +0900

    タブの動的な切り替え

--- app/assets/javascripts/change.js
+++ app/assets/javascripts/change.js
@@ -1,6 +1,10 @@
 $(document).on('turbolinks:load', function(){
-  $('.tab-list__tab').on('click', function(){
-    console.log("OK");
+
+  $('.tab-list li').on('click', function(){
+    console.log('OK');
+    var index = $('.tab-list__tab').index(this);
+    $('.tab-list__active-tab').attr('class', 'tab-list__tab');
+    $(this).attr('class', 'tab-list__active-tab');
+    console.log(index);
   });
 });
-// console.log("OK");
