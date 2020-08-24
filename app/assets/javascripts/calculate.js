7cba01e33730496d151e87a96fc49dae76711716
shwe-yuji <okumura19943210yuji@gmail.com>
2020-08-24 20:59:13 +0900

    仮の結果出力機能とリセット機能リファクタリング

--- app/assets/javascripts/calculate.js
+++ app/assets/javascripts/calculate.js
@@ -8,51 +8,47 @@ $(document).on('turbolinks:load', function(){
     return element - 0;
   };
 
-
   $('.inputform-and-result__btns--calculate-btn').on('click', function(){
+    reset();
+    // 計算に使用する数値を取得　getElement(element, text)を使用
     var monthly_deposit = 0; var monthly_deposit_text = 'monthly-deposit';
     var year = 0; var year_text = 'year';
     var month = 0; var month_text = 'month';
     var Return = 0; var Return_text = 'return';
     var final_amount = 0; var final_amount_text = 'final-amount';
     
-    console.log(getElement(monthly_deposit, monthly_deposit_text));
-    console.log(getElement(year, year_text));
-    console.log(getElement(month, month_text));
-    console.log(getElement(Return, Return_text));
-    console.log(getElement(final_amount, final_amount_text));
+    monthly_deposit = getElement(monthly_deposit, monthly_deposit_text);
+    year = getElement(year, year_text);
+    month = getElement(month, month_text);
+    Return = getElement(Return, Return_text);
+    final_amount = getElement(final_amount, final_amount_text);
 
 
-
-    // var monthly_deposits = $('.inputform-and-result__inputform__table__list--input--monthly-deposit');
-    // monthly_deposits.each(function(index, element){
-    //   monthly_deposit += $(element).val();
-    // })
-
-    // var years = $('.inputform-and-result__inputform__table__list--input--year')
-    // years.each(function(index, element){
-    //   year += $(element).val();
-    // })
-
-    // var months = $('.inputform-and-result__inputform__table__list--input--month')
-    // months.each(function(index, element){
-    //   month += $(element).val();
-    // })
-
-    // var Returns = $('.inputform-and-result__inputform__table__list--input--return')
-    // Returns.each(function(index, element){
-    //   Return += $(element).val();
-    // })
-
-    // var final_amounts = $('.inputform-and-result__inputform__table__list--input--final-amount')
-    // final_amounts.each(function(index, element){
-    //   final_amount += $(element).val();
-    // })
+    // タブ番号で処理を分岐
+    var tab_index = $('.inputform-and-result__inputform__active-table').attr('id').replace('table', "");
+    if (tab_index == 1) {
+      var result = 1;
+    } else if (tab_index == 2) {
+      var result = 2;
+    } else if (tab_index == 3) {
+      var result = 3;
+    } else if (tab_index == 4) {
+      var result = 4;
+    };
     
-    // console.log(monthly_deposit - 0);
-    // console.log(year - 0);
-    // console.log(month - 0);
-    // console.log(Return - 0);
-    // console.log(final_amount - 0);
+    console.log(result);
+    $('.inputform-and-result__result--number').prepend(result);
+
+
+
+
+
+
+    // console.log(getElement(monthly_deposit, monthly_deposit_text));
+    // console.log(getElement(year, year_text));
+    // console.log(getElement(month, month_text));
+    // console.log(getElement(Return, Return_text));
+    // console.log(getElement(final_amount, final_amount_text));
+
   });
 });
--- app/assets/javascripts/change.js
+++ app/assets/javascripts/change.js
@@ -10,11 +10,5 @@ $(document).on('turbolinks:load', function(){
     // タブの番号と同じフォームを表示
     $('.inputform-and-result__inputform__active-table').attr('class', 'inputform-and-result__inputform__table');
     $('.inputform-and-result__inputform__table').eq(tab_index-1).attr('class', 'inputform-and-result__inputform__active-table');
-
-    //  タブの切り替え時もフォームをリセットする
-    var inputs = $('.inputform-and-result__inputform__table__list--input input');
-    var result = $('.inputform-and-result__result--number');
-    inputs.val("");
-    result.empty();
   });
 });
