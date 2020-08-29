86ab9b5e44d78c89046cc6f94f1cb8724d3445f2
shwe-yuji <okumura19943210yuji@gmail.com>
2020-08-29 19:42:10 +0900

    入力欄がからの場合のアラート

--- app/assets/javascripts/calculate.js
+++ app/assets/javascripts/calculate.js
@@ -17,6 +17,16 @@ $(document).on('turbolinks:load', function(){
     $('.inputform-and-result__result--number').prepend(result);
   };
 
+  function prependPeriod(result_year, result_month){
+    if (result_year < 1) {
+      prependResult(Math.ceil(result_month) +  "ヶ月");
+    }  else if (result_month < 1) {
+      prependResult(Math.floor(result_year) + "年");
+    }  else {
+      prependResult(Math.floor(result_year) + "年" + Math.ceil(result_month) +  "ヶ月");
+    }
+  };
+
   $('.inputform-and-result__btns--calculate-btn').on('click', function(){
     // 前回に出力された値をリセット
     resultReset();
@@ -40,21 +50,48 @@ $(document).on('turbolinks:load', function(){
     // タブ番号で処理を分岐
     var tab_index = $('.inputform-and-result__inputform__active-table').attr('id').replace('table', "");
     if (tab_index == 1) {
-      // 最終積立額を計算
-      var result = monthly_deposit * (1 + monthly_Return) * ((1 + monthly_Return) ** times - 1) / (monthly_Return);
-      prependResult(Math.floor(result) + "円");
+      // 最終積立金額を計算
+      if (monthly_deposit == 0 || (year == 0 && month == 0)) {
+        window.alert("最終積立金額の算出には、毎月積立額および積立期間の入力が必須となります。ご入力いただき、再度「計算」ボタンをクリックしてください。");
+        $('.inputform-and-result__inputform__table__list--input--return').val(0);
+      } else if (Return == 0) {
+        var result = (year * 12 + month) * monthly_deposit;
+        prependResult(Math.floor(result) + "円");
+      } else {
+        var result = monthly_deposit * (1 + monthly_Return) * ((1 + monthly_Return) ** times - 1) / (monthly_Return);
+        prependResult(Math.floor(result) + "円");
+      }
 
     } else if (tab_index == 2) {
       // 毎月積立額を計算
-      var result = (final_amount * monthly_Return) / (1 + monthly_Return) / ((1 + monthly_Return) ** times - 1);
-      prependResult(Math.ceil(result) + "円");
+      if (final_amount == 0 || (year == 0 && month == 0)) {
+        window.alert("毎月積立額の算出には、最終積立金額および積立期間の入力が必須となります。ご入力いただき、再度「計算」ボタンをクリックしてください。");
+        $('.inputform-and-result__inputform__table__list--input--return').val(0);
+      } else if (Return == 0) {
+        var result = final_amount / (year * 12 + month)
+        prependResult(Math.ceil(result) + "円");
+      } else {
+        var result = (final_amount * monthly_Return) / (1 + monthly_Return) / ((1 + monthly_Return) ** times - 1);
+        prependResult(Math.ceil(result) + "円");
+      }
 
     } else if (tab_index == 3) {
       // 積立期間を計算
-      var result = Math.log((final_amount * monthly_Return) / (monthly_deposit + monthly_deposit * monthly_Return) + 1) / Math.log(1 + monthly_Return);
-      var result_year = result / 12;
-      var result_month = result % 12;
-      prependResult(Math.floor(result_year) + "年" + Math.ceil(result_month) +  "ヶ月");
+      if (final_amount == 0 || monthly_deposit == 0) {
+        window.alert("積立期間の算出には、最終積立金額および毎月積立額の入力が必須となります。ご入力いただき、再度「計算」ボタンをクリックしてください。");
+        $('.inputform-and-result__inputform__table__list--input--return').val(0);
+      } else if (Return == 0) {
+        var result = final_amount / monthly_deposit;
+        var result_year = result / 12;
+        var result_month = result % 12;
+        prependPeriod(result_year, result_month);
+
+      } else {
+        var result = Math.log((final_amount * monthly_Return) / (monthly_deposit + monthly_deposit * monthly_Return) + 1) / Math.log(1 + monthly_Return);
+        var result_year = result / 12;
+        var result_month = result % 12;
+        prependPeriod(result_year, result_month);
+      }
     };
   });
 });
